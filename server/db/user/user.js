27cc6f9773ddac3781969f6ceb368c0b87bcdb92
ericdsmith <tech.ericsmith@gmail.com>
2016-06-18 15:43:26 -0400

    create_at added

--- server/db/user/user.js
+++ server/db/user/user.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
+  created_at: {type: Date, required: true, default: Date.now}, 
   username: {type: String, required: true, unique: true}, 
   name: String,
   role: String
