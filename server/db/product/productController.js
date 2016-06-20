49d462a569af5b6a49a0fb9716db80f723de7378
ericdsmith <tech.ericsmith@gmail.com>
2016-06-20 16:29:43 -0400

    Product controller

---
+++ server/db/product/productController.js
@@ -0,0 +1,61 @@
+var Product = require('./product.js');
+
+exports.createProduct = function(newProduct, callback){
+  new Product(newProduct).save()
+  .then(function(product){
+    callback(null, product);
+  })
+  .catch(function(err){
+    callback(err);
+  });
+};
+
+exports.getAllProducts = function(callback){
+  Product.find({})
+  .then(function(products){
+    callback(products);
+  })
+  .catch(function(err){
+    callback(err);
+  });
+};
+
+exports.getProductById = function(id, callback){
+  Product.findOne({_id: id})
+  .then(function(product){
+    callback(product);
+  })
+  .catch(function(err){
+    callback(err);
+  });
+};
+
+exports.getProductByProductname = function(productname, callback){
+  Product.findOne({name: productname})
+  .then(function(product){
+    callback(product);
+  })
+  .catch(function(err){
+    callback(err);
+  });
+};
+
+exports.updateProductById = function(id, update, callback){
+  Product.findOneAndUpdate({_id: id}, update)
+  .then(function(product){
+    callback(product);
+  })
+  .catch(function(err){
+    callback(err);
+  });
+};
+
+exports.deleteProductById = function(id, callback){
+  Product.findOneAndRemove({_id: id})
+  .then(function(product){
+    callback(product);
+  })
+  .catch(function(err){
+    callback(err);
+  });
+};
