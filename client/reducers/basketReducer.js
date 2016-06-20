195590d5d3fbb3174a35b46fffe20f0efdd1928f
Eduard Bardají Puig <ebardaji@gmail.com>
2016-06-19 17:00:31 -0700

    basket reducer completed

--- client/reducers/basketReducer.js
+++ client/reducers/basketReducer.js
@@ -5,44 +5,36 @@ import * as types from '../constants/actionTypes.js'
 * An item added to the basket by inserting an object into the state array that has an `id` field and a `qty` field
 */
 
-const basketReducer = (state = [], action) => {
+const basketReducer = (state = {}, action) => {
   switch(action.type) {
 
-    // case types.BASKET_ADD_ITEM:
-    // if item already exists in basket
-      // increase count
-    // else
-      // add to basket, set count = 1
-
     case types.BASKET_DECREASE_COUNT:
-    // if item already exists in basket
-    if (var item = state.find((item) => (item.id === action.id))) {
-      if (item.qty > 1) {
-        const mapper = (item) => {
-          if (item.id === action.id) return {...item, qty: item.qty - 1}
-          else return item;
-        }
-        return state.map(mapper);
-      } else {
-        return state.filter((item) => !(item.id !== action.id))
-      }
+    if (state[action.id] > 1) {
+      return {...state, [action.id]: state[action.id] - 1};
+    } else {
+      var newState = ({...state});
+      delete newState[action.id];
+      return newState;
+    }
+
+    case types.BASKET_INCREASE_COUNT:
+    if (state[action.id]) {
+      return {...state, [action.id]: state[action.id] + 1};
+    } else {
+      return {...state, [action.id]: 1};
+    }
+
+    case types.BASKET_REMOVE_ITEM:
+    if (state[action.id]) {
+      var newState = {...state};
+      delete newState[action.id];
+      return newState;
     } else {
       return state;
     }
 
-    case types.BASKET_INCREASE_COUNT:
-    // if item already exists in basket
-      // increase count
-    // else
-      // add to basket, set count = 1
-
-    case types.BASKET_REMOVE_ITEM:
-    // if item already exists in basket
-      // remove it
-
-    case types.CHECKOUT:
-    // ?
     case types.PAYMENT_COMPLETED:
+    return {};
 
     default:
     return state;
