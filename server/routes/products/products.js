ec0539e2e5815e523ebff801f6a23845ff32d80b
ericdsmith <tech.ericsmith@gmail.com>
2016-06-20 19:33:37 -0400

    Basic product routes

--- server/routes/products/products.js
+++ server/routes/products/products.js
@@ -1,3 +1,43 @@
-var products = require('../../db/product/product.js');
+var express = require('express');
+var router = express.Router();
+var Product = require('../db/user/productController.js');
 
-app.use('/products', products);
+router.get('/', function(req, res){
+  Product.getAllProducts(function(err, products){
+    if(err){
+      res.writeHead(500);
+    } else {
+      res.writeHead(200).send(products);
+    }
+  });
+});
+
+router.post('/', function(req, res){
+  Product.createProduct(req.body, function(err, product){
+    if(err){
+      res.writeHead(500);
+    } else {
+      res.writeHead(201).send(product);
+    };
+  });
+});
+
+router.put('/:id', function(req, res){
+  Product.updateProductById(req.params.id, req.body, function(err, product){
+    if(err){
+      res.writeHead(500);
+    } else {
+      res.writeHead(200).send(product);
+    }
+  });
+});
+
+router.delete('/:id', function(req, res){
+  Product.deleteProductById(req.params.id, function(err, product){
+    if(err){
+      res.writeHead(500);
+    } else {
+      res.writeHead(200).send(product);
+    }
+  });
+});
