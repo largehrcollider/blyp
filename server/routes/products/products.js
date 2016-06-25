81db5720ed9c3fb0174bdeff5eb331af9271c12a
ericdsmith <tech.ericsmith@gmail.com>
2016-06-25 17:55:19 -0400

    Testing integrating fileupload with the api/products route

--- server/routes/products/products.js
+++ server/routes/products/products.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../../db/product/productController.js');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var fs = require('fs');
+var path = require('path');
 
 router.get('/', function(req, res){
   Product.getAllProducts(function(err, products){
@@ -12,14 +16,17 @@ router.get('/', function(req, res){
   });
 });
 
-router.post('/', function(req, res){
-  Product.createProduct(req.body, function(err, product){
-    if(err){
-      res.sendStatus(500);
-    } else {
-      res.status(201).json(product);
-    }
-  });
+router.post('/', upload.single('file'), function(req, res){
+  var filePath = req.file.path; // full path of uploaded file
+  var buff = req.file.buffer; // buffer of entire file
+  fs.rename(filePath, path.resolve(__dirname, '../uploads', '' + req.body.sku + '.jpg'), 
+    Product.createProduct(req.body, function(err, product){
+      if(err){
+        res.sendStatus(500);
+      } else {
+        res.status(201).json(product);
+      }
+    }));
 });
 
 router.get('/:sku', function(req, res){
