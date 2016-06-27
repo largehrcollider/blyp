2017a3b2c297de3634321277f8e1394e7e63faef
Steven Marks <stevenrobertmarks@gmail.com>
2016-06-27 14:37:18 -0500

    styles through checkout done

--- client/components/BasketItem.jsx
+++ client/components/BasketItem.jsx
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 // import BasketItem from '';
 // import Checkout from '';
+import material from '../assets/materialize/css/materialize.css';
+import index from '../assets/styles/index.css';
+import bootstrap from '../assets/styles/bootstrap.css';
+import inventory from '../assets/styles/InventoryPane.css';
+
 
 export default class BasketItem extends Component {
   constructor(props) {
@@ -15,18 +20,18 @@ export default class BasketItem extends Component {
     */
     const {name, quantity, price, incrementItem, decrementItem, deleteItem} = this.props;
     return (
-      <div className="basket-item">
-      <span className="name">
+      <div className={index.basket-item}>
+      <span className={index.name}>
           {name}
         </span>
-      <span className="delete" onClick={deleteItem}>X</span>
+      <span className={index.delete} onClick={deleteItem}>X</span>
         <div>
         {'($' + price*quantity + ')'}
         </div>
-        <div className="quantity">
+        <div className={index.quantity}>
         <span> {'Quantity: '+quantity+' '}</span>
-        <span className="increment-up" onClick={incrementItem}>+</span>
-        <span className="increment-down" onClick={decrementItem}>-</span>
+        <span className={index.incrementUp} onClick={incrementItem}>+</span>
+        <span className={index.incrementDown} onClick={decrementItem}>-</span>
         </div>
         {<br></br>}
         
--- client/components/CashSelector.jsx
+++ client/components/CashSelector.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import material from '../assets/materialize/css/materialize.css';
+import index from '../assets/styles/index.css';
+import bootstrap from '../assets/styles/bootstrap.css';
+import inventory from '../assets/styles/InventoryPane.css';
+
 
 const MethodCash = ({ selectCash }) => (
-  <div className="pay-button btn-success" onClick={selectCash}>
+  <div className={index.payButton} onClick={selectCash}>
     Cash
   </div>
 );
