cdcd4ef2995420d2ac9668a66df2f771af159b93
ericdsmith <tech.ericsmith@gmail.com>
2016-06-28 14:13:29 -0400

    Posts to employment are working

--- server/routes/employment/employment.js
+++ server/routes/employment/employment.js
@@ -6,68 +6,72 @@ var User = require('../../db/user/userController.js');
 
 router.post('/', function(req, res){
   var token = req.params.token;
-  var user = token.username;
+  var user = req.body.user;
+  //var user = token.username;
   var isAdmin = false;
+  var businessName = req.body.businessName;
 
-  Business.getBusinessByName(req.body.business, function(err, business){
+  Business.getBusinessByName(businessName, function(err, business){
     if(err){
       res.sendStatus(500);
     } else {
       //Ensure the user has admin permissions
       for(var m = 0; m < business.users.length; m++){
-        if(business.users[m].username === user && business.users[m].role === 'admin'){
+        if(business.users[m].user_id === user && business.users[m].role === 'admin'){
           isAdmin = true;
         }
       }
+
+      if(isAdmin){
+        if(req.body.status === 'accept'){
+          User.getUserByUsername(req.body.username, function(err, user){
+            if(err){
+              res.sendStatus(500);
+            } else {
+              Business.getBusinessByName(businessName, function(err, business){
+                if(err){
+                  res.sendStatus(500);
+                } else {
+                  user.businesses.push(businessName);
+                  user.save();
+                  business.users.push({user_id: req.body.username, role: 'cashier'});
+                  business.save();
+                  res.status(200).json(business);
+                }
+              });
+            }
+          });
+        } else {
+          User.getUserByUsername(req.username, function(err, user){
+            if(err){
+              res.sendStatus(500);
+            } else {
+              Business.getBusinessByName(businessName, function(err, business){
+                if(err){
+                  res.sendStatus(500);
+                } else {
+                  //Remove the request from the business
+                  for(var i = 0; i < business.requests.length; i++){
+                    if(business.requests[i] === req.username){
+                      business.requests.splice(i, 1);
+                    }
+                  }
+                  //Remove the request from the user
+                  for(var j = 0; j < user.requests.length; j++){
+                    if(user.requests[i] === businessName){
+                      user.requests.splice(i, 1);
+                    }
+                  }
+                }
+              });
+            }
+          });
+        }
+      } else {
+        res.status(500).send('You are not an admin for this business!');
+      }
     }
   });
-
-  if(isAdmin){
-    if(req.body.status === 'accept'){
-      User.getUserByUsername(req.username, function(err, user){
-        if(err){
-          res.sendStatus(500);
-        } else {
-          Business.getBusinessByName(req.body.business, function(err, business){
-            if(err){
-              res.sendStatus(500);
-            } else {
-              user.businesses.push(req.body.business);
-              business.users.push({id: req.username, role: 'cashier'});
-              res.status(200).json(business);
-            }
-          });
-        }
-      });
-    } else {
-      User.getUserByUsername(req.username, function(err, user){
-        if(err){
-          res.sendStatus(500);
-        } else {
-          Business.getBusinessByName(token.business, function(err, business){
-            if(err){
-              res.sendStatus(500);
-            } else {
-              //Remove the request from the business
-              for(var i = 0; i < business.requests.length; i++){
-                if(business.requests[i] === req.username){
-                  business.requests.splice(i, 1);
-                }
-              }
-              //Remove the request from the user
-              for(var j = 0; j < user.requests.length; j++){
-                if(user.requests[i] === token.business){
-                  user.requests.splice(i, 1);
-                }
-              }
-            }
-          });
-        }
-      });
-    }
-  } else {
-    res.status(500).send('You are not an admin for this business!');
-  }
 });
 
 router.get('/:name', function(req, res){
