aaa7814a1e01d741456130ef3e2ccd434c1c58f9
ericdsmith <tech.ericsmith@gmail.com>
2016-06-29 14:07:06 -0400

    Instructions for disabling auth for devlopment.

--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -5,12 +5,15 @@ var router = require('express').Router();
 var User = require('../db/user/userController.js');
 
 module.exports = router.post('/', (req, res) => {
-  // var token = jwt.sign({
-  //   admin: true,
-  //   name: 'Eduard',
-  //   username: 'edu',
-  // }, SECRET);
+/*  Uncomment the lines below if you'd like to disable Auth for development.
+  var token = jwt.sign({
+    admin: true,
+    name: 'Eduard',
+    username: 'edu',
+  }, SECRET);
+  res.json({admin: true, jwt: token, name: 'Eduard', username: 'edu'});*/
 
+//Comment out the block of code below if you'd like to disable Auth for development.
   User.getUserByUsername(req.body.username, (err, user) => {
     if(err){
       res.sendStatus(500);
@@ -28,5 +31,4 @@ module.exports = router.post('/', (req, res) => {
       });
     }
   });
-  //res.json({admin: true, jwt: token, name: 'Eduard', username: 'edu'});
 });
