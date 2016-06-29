ba329a591333db35a57be95abaf668e1942ee868
ericdsmith <tech.ericsmith@gmail.com>
2016-06-29 02:21:36 -0400

    Create user with password. Login will correctly compare against saved password.

--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -2,6 +2,7 @@ var jwtParser = require('express-jwt');
 var jwt = require('jsonwebtoken');
 var SECRET = require('../../keys/secret.js');
 var router = require('express').Router();
+var User = require('../db/user/userController.js');
 
 module.exports = router.post('/', (req, res) => {
   var token = jwt.sign({
@@ -10,5 +11,23 @@ module.exports = router.post('/', (req, res) => {
     username: 'edu',
   }, SECRET);
 
-  res.json({admin: true, jwt: token, name: 'Eduard', username: 'edu'});
+User.getUserByUsername(req.body.username, (err, user) => {
+  if(err){
+    res.sendStatus(500);
+  } else if(!user){
+    res.status(500).send('User does not exist');
+  } else {
+    user.comparePasswords(req.body.password)
+    .then(function(isMatch){
+      console.log(isMatch)
+      if(isMatch){
+        var token = jwt.sign({username: req.username}, SECRET);
+        res.status(200).json(token);
+      } else {
+        res.sendStatus(500);
+      }
+    })
+  }
+});
+  //res.json({admin: true, jwt: token, name: 'Eduard', username: 'edu'});
 });
