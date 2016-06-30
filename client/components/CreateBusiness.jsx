070de15656b84d88ac1ca4627ea93bad341d34a1
Steven Marks <stevenrobertmarks@gmail.com>
2016-06-30 13:58:25 -0500

    styled create and join business components WITHOUT changing buttons

--- client/components/CreateBusiness.jsx
+++ client/components/CreateBusiness.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import index from '../assets/styles/index.css';
 
 export default class CreateBusiness extends Component {
   constructor(props) {
@@ -9,8 +10,8 @@ export default class CreateBusiness extends Component {
     return (
       <div>
         <form onSubmit={handleSubmit(create)}>
-          <div>
-            <label>Business to create: <input type='text' {...business}/></label>
+          <div >
+            <label className={index.createProductLabel}>Business to create: <input className={index.createProduct} type='text' {...business}/></label>
           </div>
           <div className=''>
             <input type='submit' value='CREATE' />
