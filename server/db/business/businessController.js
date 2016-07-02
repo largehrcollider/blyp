daeb1608ee83da1f8a0592800820b6f0409d592e
ericdsmith <tech.ericsmith@gmail.com>
2016-07-02 19:05:08 -0400

    Added function for updating an employment request

--- server/db/business/businessController.js
+++ server/db/business/businessController.js
@@ -2,14 +2,13 @@
 var Business = require('./business.js');
 
 exports.createBusiness = function(reqBody, tokenPayload, callback){
-  var newBusiness = {
-    name: reqBody.business,
-    users: [{username: tokenPayload.username, role: 'admin'}]
-  };
+  var newBusiness = reqBody;
+  newBusiness.name = reqBody.business,
+  newBusiness.users = [{username: tokenPayload.username, role: 'admin'}];
   new Business(newBusiness).save(callback);
 };
 
-exports.getAllBusinesss = function(callback){
+exports.getAllBusinesses = function(callback){
   Business.find({}, callback);
 };
 
@@ -21,6 +20,33 @@ exports.deleteBusinessByName = function(name, callback){
   Business.findOneAndRemove({name: name}, callback);
 };
 
-exports.employementRequest = function(username, businessName, status, callback){
-  Business.findOne({name: businessName}, callback);
+/**
+ * 
+ * @param {string} business - 
+ * @param {string} sku - 
+ * @param {function} callback - 
+ */
+exports.updateEmploymentRequest = function(username, businessName, status, callback){
+  Business.findOne({name: businessName}, function(err, business){
+    if(err){
+      callback(err);
+    } else {
+      var index = business.requests.indexOf(username);
+      if(index !== -1){
+        if(status === 'accept'){
+          business.users.push({username: username, role: 'cashier'});
+          business.requests.splice(index, 1);
+          business.save(function(){
+            callback(null, business.users);
+          });
+        } else {
+          business.requests.splice(index, 1);
+          business.save();
+          callback(null, business);
+        }
+      } else {
+        callback("The user's request was not found with the business!");
+      }
+    }
+  });
 };
