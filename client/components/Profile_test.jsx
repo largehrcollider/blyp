20bbf306211a7cf60ff6639cd97d8b61394d1fc0
leov2000 <leovergarah@gmail.com>
2016-07-03 02:06:10 -0400

    added(updated user info functionality to component)

--- client/components/Profile_test.jsx
+++ client/components/Profile_test.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import SelectBusiness from '../containers/SelectBusiness.js';
 import JoinBusiness from '../containers/JoinBusiness';
 import CreateBusiness from '../containers/CreateBusiness';
-import ChangeProfileName from '../containers/ProfileNameButton.js';
+import ChangeProfileName from '../containers/ProfileUsernameButton.js';
 import ChangePassword from '../containers/ProfilePasswordButton.js';
-import ChangeName from '../containers/ProfileUsernameButton.js';
-import ChangeEmail from '../containers/ProfileUsernameButton.js';
+import ChangeName from '../containers/ProfileNameButton.js';
+import ChangeEmail from '../containers/ProfileEmailButton.js';
 import bootstrap from '../assets/styles/bootstrap.css';
 import material from '../assets/materialize/css/materialize.css';
 import index from '../assets/styles/index.css';
 import inventory from '../assets/styles/InventoryPane.css';
+import UpdateName from'../containers/UpdateNameForm';
+import UpdateEmail from '../containers/UpdateEmailForm';
+import UpdatePassword from '../containers/UpdatePasswordForm';
+import UpdateUsername from '../containers/UpdateUsernameForm';
+// import ProfileDetails from 'profileDetails.jsx';
+// import ProfileDetails from '../components/ProfileDetails.jsx';
 
-let classes = ""
 
-
-const Profile = ({ businesses }) => (
+const Profile = ({ businesses, name, username, email, changeName, changeEmail, changeUsername, changePassword}) => (
   <div>
     <div className={bootstrap.colmd6}>
       {businesses.map(b => (
@@ -24,20 +28,24 @@ const Profile = ({ businesses }) => (
         <CreateBusiness />
     </div>
     {/*Profile info starts here */}
-      <div className={index.profilebox}>
-        <div className={index.profilepic}/>
-        <div className={index.container}>
-          <div className={bootstrap.colmd4}>
-            <div className={index.profilerow}>User Name:<br/></div>
-            <div className={index.profilerow}>User ProfileName:<br/></div>
-            <div className={index.profilerow}>User Email:<br/></div>
-            <div className={index.profilerow}>Password<br/></div>
-          </div>
-        <div className={bootstrap.colmd2}>
+    <div className={index.profilebox}>
+      <div className={index.profilepic}/>
+      <div className={index.container}>
+        <div className={bootstrap.colmd3}>
+          <div className={index.profilerow}>Name:{name}<br/></div>
+          <div className={index.profilerow}>Username:{username}<br/></div>
+          <div className={index.profilerow}>Email:{email}<br/></div>
+          <div className={index.profilerow}>Password<br/></div>
+        </div>
+        <div className={bootstrap.colmd3}>
           <div className={index.profilebuttonrow}><ChangeName /></div>
+          {changeName && <UpdateName/>}
           <div className={index.profilebuttonrow}><ChangeProfileName /></div>
+          {changeUsername && <UpdateUsername/>}
           <div className={index.profilebuttonrow}><ChangeEmail /></div>
+          {changeEmail && <UpdateEmail/>}
           <div className={index.profilebuttonrow}><ChangePassword /></div>
+          {changePassword && <UpdatePassword/>}
         </div>
       </div>
     </div>
