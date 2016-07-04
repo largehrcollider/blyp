4aefcfd71ac2f3d59a42c481a6e1b25004b826a2
ericdsmith <tech.ericsmith@gmail.com>
2016-07-04 14:10:40 -0400

    Added adduser, removeuser, updateuserrole and addemployement request to businesscontroller

--- server/db/business/businessController.js
+++ server/db/business/businessController.js
@@ -3,7 +3,7 @@ var Business = require('./business.js');
 
 exports.createBusiness = function(reqBody, tokenPayload, callback){
   var newBusiness = reqBody;
-  newBusiness.name = reqBody.business,
+  //newBusiness.name = reqBody.business,
   newBusiness.users = [{username: tokenPayload.username, role: 'admin'}];
   new Business(newBusiness).save(callback);
 };
@@ -18,6 +18,47 @@ exports.getBusinessByName = function(name, callback){
 
 exports.deleteBusinessByName = function(name, callback){
   Business.findOneAndRemove({name: name}, callback);
+};
+
+exports.addUser = function(businessName, userObj, callback){
+  Business.findOne({name: businessName}, function(err, business){
+    if(err){
+      callback(err);
+    } else {
+      business.users.push(userObj);
+      business.save(callback);  
+    }
+  });
+};
+
+exports.removeUser = function(businessName, username, callback){
+  Business.findOne({name: businessName}, function(err, business){
+    if(err){
+      callback(err);
+    } else {
+      for(var i = 0; i < business.users.length; i++){
+        if(business.users[i].username === username){
+          business.users.splice(i, 1);
+          business.save(callback);
+        }
+      }
+    }
+  });
+};
+
+exports.updateUserRole = function(businessName, userObj, callback){
+  Business.findOne({name: businessName}, function(err, business){
+    if(err){
+      callback(err);
+    } else {
+      for(var i = 0; i < business.users.length; i++){
+        if(business.users[i].username === userObj.username){
+          business.users[i].role = userObj.role;
+          business.save(callback);
+        }
+      }
+    }
+  });
 };
 
 /**
@@ -41,12 +82,25 @@ exports.updateEmploymentRequest = function(username, businessName, status, callb
           });
         } else {
           business.requests.splice(index, 1);
-          business.save();
-          callback(null, business);
+          business.save(callback);
         }
       } else {
         callback("The user's request was not found with the business!");
       }
+    }
+  });
+};
+
+exports.addEmploymentRequest = function(username, businessName, callback){
+  Business.findOne({name: businessName}, function(err, business){
+    if(err){
+      callback(err);
+    } else {
+      if(business){
+        business.requests.push(username);
+        business.save();
+      }
+      callback(null, business);
     }
   });
 };
