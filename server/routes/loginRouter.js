c886ab4a2a70b2a9fceb7e18ef8029635db7e724
Eduard Bardají Puig <ebardaji@gmail.com>
2016-07-03 22:43:54 -0700

    adds a few more logs

--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -15,13 +15,12 @@ module.exports = router.post('/', (req, res) => {
 
 //Comment out the block of code below if you'd like to disable Auth for development.
   User.getUserByUsername(req.body.username, (err, user) => {
-    console.log(req.body.username)
     if(err){
-      console.log('Error retrieving user from db');
+      console.log('Error retrieving user from db.');
       console.log(err.message);
       res.sendStatus(500);
     } else if(!user){
-      console.log(`User ${req.body.username} does not exist in the db`);
+      console.log(`User ${req.body.username} does not exist in the db.`);
       res.sendStatus(403);
     } else {
       user.comparePasswords(req.body.password)
