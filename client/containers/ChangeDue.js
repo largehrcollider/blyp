1530fe8cc12a4539f7303ee2de87b94a645c0bf1
leov2000 <leovergarah@gmail.com>
2016-07-05 02:26:00 -0400

    refactored(container to redirect to receipt)

--- client/containers/ChangeDue.js
+++ client/containers/ChangeDue.js
@@ -11,8 +11,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    checkoutCompleted: () => {
-      dispatch(actions.checkoutCompleted());
+    receiptPage: () => {
+      dispatch(actions.receiptPage());
     }
   };
 }
